4ac8f0ae0e64413211a2257c58baa15696c1a6aa
Adem-elwafi <adem.elwafi.dev@gmail.com>
2026-03-25 20:51:41 +0100

    feat: initial project scaffolding with Spring Boot 3.2 and JJWT

---
+++ src/test/java/com/collabcode_api/CollabcodeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collabcode_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollabcodeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
